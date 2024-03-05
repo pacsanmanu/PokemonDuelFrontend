e6de0f2d799b3b20ac0b74b5ef27671db4ffab03
Manuel Pacheco <mpdevlp@gmail.com>
2024-03-05 09:59:28 +0100

    New sprites & image sizing logic implemented

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,12 +1,47 @@
+import React from 'react';
 import './PokemonDetails.css';
 
 const PokemonDetails = ({ role, pokemon, onAttack }) => {
+
+  const handleImageLoad = (event) => {
+    const img = event.target;
+    const naturalHeight = img.naturalHeight;
+    const naturalWidth = img.naturalWidth;
+    const maxHeight = 150; // El alto máximo permitido
+
+    let newHeight = naturalHeight;
+    let newWidth = naturalWidth;
+
+    // Reescalar basándonos únicamente en el alto
+    if (naturalHeight > maxHeight) {
+      // Si el alto supera el máximo, ajustamos manteniendo la proporción
+      const scale = maxHeight / naturalHeight;
+      newHeight = maxHeight; // Limitamos el alto a 150px
+      newWidth = naturalWidth * scale; // Ajustamos el ancho proporcionalmente
+    } else if (naturalHeight < 50) {
+      // Para imágenes muy pequeñas, aumentamos su tamaño
+      const increaseFactor = 2; // Aumento para imágenes muy pequeñas
+      newHeight = naturalHeight * increaseFactor;
+      newWidth = naturalWidth * increaseFactor;
+    } else if (naturalHeight < 100) {
+      // Para imágenes pequeñas, aplicamos un incremento moderado
+      const increaseFactor = 1.3; // Incremento moderado para imágenes pequeñas
+      newHeight = naturalHeight * increaseFactor;
+      newWidth = naturalWidth * increaseFactor;
+    }
+
+    // Ajustar las dimensiones de la imagen
+    img.style.height = `${newHeight}px`;
+    img.style.width = `${newWidth}px`;
+  };
+
   return (
     <div className="pokemon-details">
       <img
         className={role === 'user' ? 'user-pokemon' : 'ai-pokemon'}
         src={`${process.env.PUBLIC_URL}/images/sprites/${pokemon.pokedexId}.gif`}
         alt={pokemon.name}
+        onLoad={handleImageLoad}
       />
       <h2>{pokemon.name}</h2>
       <p>HP: {pokemon.stats.life}</p>
