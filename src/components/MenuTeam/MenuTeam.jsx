5593d21ea93e9396891636f09c08b7a33cce6335
Manuel Pacheco <mpdevlp@gmail.com>
2024-05-30 19:11:30 +0200

    Login and register styles completed

--- src/components/MenuTeam/MenuTeam.jsx
+++ src/components/MenuTeam/MenuTeam.jsx
@@ -1,18 +1,29 @@
+import React from 'react';
+import './MenuTeam.css';
+
 const MenuTeam = ({ userTeam, evolutions, onEvolvePokemon, onRemovePokemon, userCoins }) => (
-  <div>
+  <div className="menu-team">
     <h2>Your Team (Coins: {userCoins})</h2>
-    <ul>
+    <ul className="pokemon-list">
       {userTeam.map((pokemon, index) => (
-        <li key={index}>
-          {pokemon}
-          {evolutions[pokemon] && evolutions[pokemon].evolution && (
+        <li key={index} className="pokemon-item">
+          <img
+            src={`../../public/images/sprites/${pokemon.pokedexId}.gif`}
+            alt={pokemon.name}
+            className="pokemon-image"
+          />
+          <span className="pokemon-name">{pokemon.name}</span>
+          {evolutions[pokemon.name] && evolutions[pokemon.name].evolution && (
             <>
-              <button onClick={() => onEvolvePokemon(index)}>
-                Evolve to {evolutions[pokemon].evolution} for {evolutions[pokemon].cost} coins
+              <button
+                className="evolve-button"
+                onClick={() => onEvolvePokemon(index)}
+              >
+                Evolve to {evolutions[pokemon.name].evolution} for {evolutions[pokemon.name].cost} coins
               </button>
             </>
           )}
-          <button onClick={() => onRemovePokemon(index)}>Remove</button>
+          <button className="remove-button" onClick={() => onRemovePokemon(index)}>Remove</button>
         </li>
       ))}
     </ul>
