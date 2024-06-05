ed50d38c1629284b6517208f1a965f39fc1559bf
Manuel Pacheco <mpdevlp@gmail.com>
2024-06-05 17:50:47 +0200

    Removed sprites backup

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,6 +3,7 @@ const Shop = ({ pokemonsToBuy, onBuyPokemon }) => (
     <h2>Pokemons to Buy</h2>
     {pokemonsToBuy.map((pokemon) => (
       <button key={pokemon._id} onClick={() => onBuyPokemon(pokemon.name)}>
+        <img src={`images/sprites/${pokemon.pokedexId}.gif`} alt={pokemon.name} />
         Buy {pokemon.name} for {pokemon.price} coins
       </button>
     ))}
